68a2e094041846d30ab4165abc3d129cd7e04ca6
nagasawaaaa <13200956+nagasawaaaa@users.noreply.github.com>
2017-08-10 11:33:32 +0900

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -1,49 +1,30 @@
+'use strict';
+
 Vue.component('task-field', {
-  template: `<li class="list-group-item">
-    <p>{{ parentTask }}</p>
-    <div class="form-group" v-if="edit">
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Edit task..." v-model="newTask">
-        <span class="input-group-btn">
-          <button class="btn btn-default" type="button" @click="updateTask">Done</button>
-        </span>
-      </div>
-    </div>
-    <div class="text-right">
-      <div class="btn-group" role="group" v-if="!edit">
-        <button type="button" class="btn btn-success" @click="editToggle">Edit</button>
-      </div>
-      <div class="btn-group" role="group" v-if="edit">
-        <button type="button" class="btn btn-success" @click="editCancel">Cancel</button>
-      </div>
-      <div class="btn-group" role="group">
-        <button type="button" class="btn btn-danger" @click="getIndex">Delete</button>
-      </div>
-    </div>
-  </li>`,
+  template: '<li class="list-group-item">\n    <p>{{ parentTask }}</p>\n    <div class="form-group" v-if="edit">\n      <div class="input-group">\n        <input type="text" class="form-control" placeholder="Edit task..." v-model="newTask">\n        <span class="input-group-btn">\n          <button class="btn btn-default" type="button" @click="updateTask">Done</button>\n        </span>\n      </div>\n    </div>\n    <div class="text-right">\n      <div class="btn-group" role="group" v-if="!edit">\n        <button type="button" class="btn btn-success" @click="editToggle">Edit</button>\n      </div>\n      <div class="btn-group" role="group" v-if="edit">\n        <button type="button" class="btn btn-success" @click="editCancel">Cancel</button>\n      </div>\n      <div class="btn-group" role="group">\n        <button type="button" class="btn btn-danger" @click="getIndex">Delete</button>\n      </div>\n    </div>\n  </li>',
   props: {
     'parentTask': String,
     'id': Number
   },
-  data: function(){
+  data: function data() {
     return {
       edit: false,
       newTask: ''
-    }
+    };
   },
   methods: {
-    editToggle: function(){
+    editToggle: function editToggle() {
       this.edit = !this.edit;
     },
-    editCancel: function(){
+    editCancel: function editCancel() {
       this.editToggle();
       this.newTask = "";
     },
-    getIndex: function(){
+    getIndex: function getIndex() {
       this.$emit('get-index', this.id);
     },
-    updateTask: function(){
-      if(this.newTask === "") return;
+    updateTask: function updateTask() {
+      if (this.newTask === "") return;
       this.$emit('update-task', this.id, this.newTask);
       this.editCancel();
     }
@@ -57,16 +38,15 @@ var vm = new Vue({
     tasks: []
   },
   methods: {
-    sendTask: function(){
-      if( this.text === '' ) return;
+    sendTask: function sendTask() {
+      if (this.text === '') return;
       this.tasks.push(this.text);
       this.text = '';
-
     },
-    deleteTask: function(id){
-      this.tasks.splice(id,1);
+    deleteTask: function deleteTask(id) {
+      this.tasks.splice(id, 1);
     },
-    updateTask: function(id, task){
+    updateTask: function updateTask(id, task) {
       this.tasks.splice(id, 1, task);
     }
   }
